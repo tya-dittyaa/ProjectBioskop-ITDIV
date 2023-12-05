18eecc1008037e158acb23f2a01c2897be9460aa
Andrew7-7 <duduandrew7@gmail.com>
2023-12-05 20:12:07 +0700

    update baru

--- Frontend/src/pages/movieList/MovieList.tsx
+++ Frontend/src/pages/movieList/MovieList.tsx
@@ -1,79 +1,81 @@
-import NavBar from '../assets/NavBar';
-import './movieList.css'
-import { Link } from 'react-router-dom'
-import {useState} from 'react'
-export default function MovieList(){
-  const [movies,setMovies] = useState([]);
-  
-    return (
-      <>
-      <NavBar/>
-        <div className="movieList">
-          <div className="nowPlaying">
-            <h1>NOW SHOWING</h1>
-            <div className="nowPlayingContainer">
-              <div className="nowPlayingList">
-                <img src="1.jpg" alt="" />
-                <span>
-                  THE HUNGER GAMES: THE BALLAD OF SONGBIRDS AND SNAKES (IMAX 2D)
-                </span>
-              </div>
-              <div className="nowPlayingList">
-                <img src="2.jpg" alt="" />
-                <span>
-                  THE HUNGER GAMES: THE BALLAD OF SONGBIRDS AND SNAKES
-                </span>
-              </div>
-              <div className="nowPlayingList">
-                <img src="3.jpg" alt="" />
-                <span>SIJJIN</span>
-              </div>
-              <div className="nowPlayingList">
-                <img src="4.jpg" alt="" />
-                <span>RUMAH IBLIS</span>
-              </div>
-              <div className="nowPlayingList">
-                <img src="5.jpg" alt="" />
-                <span>172 DAYS</span>
-              </div>
-            </div>
-          </div>
+import NavBar from "../assets/NavBar";
+import "./movieList.css";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+export default function MovieList() {
+  const [movies, setMovies] = useState([]);
+  const [comings, setComings] = useState([]);
+  useEffect(() => {
+    const getMovie = async () => {
+      try {
+        const response = await fetch(
+          "https://api-bioskop13.dittyaa.my.id/film/available",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer fredjefdrewkardit",
+            },
+          }
+        );
+        const data = await response.json();
+        setMovies(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const getComing = async () => {
+      try {
+        const response = await fetch(
+          "https://api-bioskop13.dittyaa.my.id/film/comingsoon",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Bearer fredjefdrewkardit",
+            },
+          }
+        );
+        const data = await response.json();
+        setComings(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getMovie();
+    getComing();
+  }, []);
 
-          <div className="comingSoon">
-            <h1>COMING SOON</h1>
-            <div className="comingSoonContainer">
-              <Link to="">
-                <div className="comingSoonList">
-                  <img src="1.jpg" alt="" />
-                  <span>
-                    THE HUNGER GAMES: THE BALLAD OF SONGBIRDS AND SNAKES (IMAX
-                    2D)
-                  </span>
+  return (
+    <>
+      <NavBar />
+      <div className="movieList">
+        <div className="nowPlaying">
+          <h1>NOW SHOWING</h1>
+          <div className="nowPlayingContainer">
+            {movies.map((movie) => (
+              <Link to={`/bioskop/${movie.id}`} key={movie.id}>
+                <div className="nowPlayingList">
+                  <img src={movie.image_link} alt="" />
+                  <span>{movie.title}</span>
                 </div>
               </Link>
-
-              <div className="comingSoonList">
-                <img src="2.jpg" alt="" />
-                <span>
-                  THE HUNGER GAMES: THE BALLAD OF SONGBIRDS AND SNAKES
-                </span>
-              </div>
-
-              <div className="comingSoonList">
-                <img src="3.jpg" alt="" />
-                <span>SIJJIN</span>
-              </div>
-              <div className="comingSoonList">
-                <img src="4.jpg" alt="" />
-                <span>RUMAH IBLIS</span>
-              </div>
-              <div className="comingSoonList">
-                <img src="5.jpg" alt="" />
-                <span>172 DAYS</span>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
-      </>
-    );
+
+        <div className="comingSoon">
+          <h1>COMING SOON</h1>
+          <div className="comingSoonContainer">
+            {comings.map((coming) => (
+              <div className="comingSoonList" key={coming.id}>
+                <img src={coming.image_link} alt="" />
+                <span>{coming.title}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
