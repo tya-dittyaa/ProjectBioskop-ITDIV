586c7ecadb511cc07fdb5c7ffa8193ab7e9bdb41
Aditya Fajri Isnandar <67040804+tya-dittyaa@users.noreply.github.com>
2023-12-07 13:37:28 +0700

    push payment image, done :>

--- Backend/src/controllers/payment.controller.ts
+++ Backend/src/controllers/payment.controller.ts
@@ -6,15 +6,15 @@ const prisma = new PrismaClient();
 export class PaymentController {
   // * Add
   static async add(req: Request, res: Response) {
-    const { name } = req.body;
+    const { name, image } = req.body;
 
     // ! Req Body is Missing
-    if (!name) {
+    if (!name || !image) {
       return res.status(400).json({ message: "Request body is missing" });
     }
 
     const dataCreate = await prisma.paymentMethod.create({
-      data: { name },
+      data: { name, image },
     });
 
     return res.status(201).json({
